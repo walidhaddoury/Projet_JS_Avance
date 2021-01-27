a36b6ce355d987cb0f6d52a5a2fbc382f7ef081e
walid <walidhaddoury@outlook.fr>
2021-01-27 19:29:16 +0100

    modification du localStorage

--- ProjetDepart/js/app.js
+++ ProjetDepart/js/app.js
@@ -4,21 +4,73 @@ const cartButton = document.querySelectorAll('.add-to-cart'); // All buttons Add
 const emptyCart = document.querySelector('#empty-cart');  // Button to reset Cart
 const listCart = document.querySelector('#cart-table tbody'); // liste of product in Cart
 
+let panier = JSON.parse(localStorage.getItem('Cart')) || [];
+
 const productsTitle = document.querySelectorAll('.course__item .info__card h4');
 const productsImg = document.querySelectorAll('.course__item .course_img img');
 const productsPrice = document.querySelectorAll('.course__item .info__card .discount')
 
 //localStorage values
 let uxUI = localStorage.getItem('UX/UI');
-console.log(uxUI);
+//console.log(uxUI);
 let php = localStorage.getItem('php');
-console.log(php);
+//console.log(php);
 let react = localStorage.getItem('react');
-console.log(react);
+//console.log(react);
 let node = localStorage.getItem('node');
-console.log(node);
+//console.log(node);
 let mySQL = localStorage.getItem('mySQL');
-console.log(mySQL);
+//console.log(mySQL);
+
+
+function loadLocalStorage() {
+    if (localStorage.getItem('Panier')) {
+        for (let i = 0; i < JSON.parse(localStorage.getItem('Panier').length); i++) {
+            const getTitle = JSON.parse(localStorage.getItem('Panier')[i].title);
+            const getPrice = JSON.parse(localStorage.getItem('Panier')[i].price);
+            const getImg = JSON.parse(localStorage.getItem('Panier')[i].img);
+            const getStock = JSON.parse(localStorage.getItem('Panier')[i].stock);
+
+
+            const newProduct = document.createElement('tr');
+
+            let imgProduct = document.createElement('td');
+            let img = document.createElement('img');
+            img.setAttribute('src', getImg);
+            imgProduct.appendChild(img);
+
+            let titleProduct = document.createElement('td');
+            titleProduct.innerHTML = getTitle;
+
+            let priceProduct = document.createElement('td');
+            priceProduct.innerHTML = getPrice;
+
+            let removeProduct = document.createElement('a');
+            removeProduct.addEventListener('click', removeFromCart);
+            let imgRemove = document.createElement('img');
+            imgRemove.setAttribute('src', './img/fermer.svg');
+            removeProduct.appendChild(imgRemove);
+
+            let stock = document.createElement('td');
+            stock.innerHTML = getStock;
+
+            newProduct.appendChild(imgProduct);
+            newProduct.appendChild(titleProduct);
+            newProduct.appendChild(priceProduct);
+            newProduct.appendChild(stock);
+            newProduct.appendChild(removeProduct);
+
+            listCart.appendChild(newProduct);
+
+
+
+        }
+    }
+    console.log("Le panier dans le LocalStorage est vide !");
+}
+
+
+
 
 
 for (let i = 0; i < cartButton.length; i++) {
@@ -29,6 +81,20 @@ for (let i = 0; i < cartButton.length; i++) {
 
 
     cartButton[i].addEventListener('click', () => {
+
+        let article = {
+            img: imgCart,
+            title: titleCart,
+            price: priceCart,
+            stock: 1
+        };
+        panier.push(article);
+
+        localStorage.setItem('Panier', JSON.stringify(panier));
+
+        console.log(titleCart);
+        console.log(priceCart);
+        console.log(imgCart);
 
         const newProduct = document.createElement('tr');
 
@@ -44,6 +110,7 @@ for (let i = 0; i < cartButton.length; i++) {
         let imgRemove = document.createElement('img');
 
         let stock = document.createElement('td');
+        stock.innerHTML = 1;
 
         img.setAttribute('src', productsImg[i].getAttribute('src'));
         imgProduct.appendChild(img);
@@ -54,54 +121,52 @@ for (let i = 0; i < cartButton.length; i++) {
         titleProduct.innerHTML = titleCart;
         priceProduct.innerHTML = priceCart;
 
+        newProduct.appendChild(imgProduct);
+        newProduct.appendChild(titleProduct);
+        newProduct.appendChild(priceProduct);
+        newProduct.appendChild(stock);
+        newProduct.appendChild(removeProduct);
+
+        listCart.appendChild(newProduct);
+
 
         //console.log(titleCart);
         //console.log(priceCart);
         //console.log(imgCart);
-
-        switch (titleCart) {
-            case "UX/UI":
-                uxUI++;
-                localStorage.setItem('UX/UI', uxUI);
-                stock.innerHTML = localStorage.getItem('UX/UI');
-                break;
-            case "PHP 8":
-                php++;
-                localStorage.setItem('php', php);
-                stock.innerHTML = localStorage.getItem('php');
-                break;
-            case "React JS":
-                react++;
-                localStorage.setItem('react', react);
-                stock.innerHTML = localStorage.getItem('react');
-                break;
-            case "Node JS":
-                node++;
-                localStorage.setItem('node', node);
-                stock.innerHTML = localStorage.getItem('node');
-                break;
-            case "MySQL":
-                mySQL++;
-                localStorage.setItem('mySQL', mySQL);
-                stock.innerHTML = localStorage.getItem('mySQL');
-                break;
-            default:
-                break;
-        }
-
-        if (uxUI < 1 || php < 1 || react < 1 || node < 1 || mySQL < 1) {
-            newProduct.appendChild(imgProduct);
-            newProduct.appendChild(titleProduct);
-            newProduct.appendChild(priceProduct);
-            newProduct.appendChild(stock);
-            newProduct.appendChild(removeProduct);
-
-            listCart.appendChild(newProduct);
-        }
-
+        /*
+                switch (titleCart) {
+                    case "UX/UI":
+                        uxUI++;
+                        localStorage.setItem('UX/UI', uxUI);
+                        //stock.innerHTML = localStorage.getItem('UX/UI');
+                        break;
+                    case "PHP 8":
+                        php++;
+                        localStorage.setItem('php', php);
+                        //stock.innerHTML = localStorage.getItem('php');
+                        break;
+                    case "React JS":
+                        react++;
+                        localStorage.setItem('react', react);
+                        //stock.innerHTML = localStorage.getItem('react');
+                        break;
+                    case "Node JS":
+                        node++;
+                        localStorage.setItem('node', node);
+                        //stock.innerHTML = localStorage.getItem('node');
+                        break;
+                    case "MySQL":
+                        mySQL++;
+                        localStorage.setItem('mySQL', mySQL);
+                        //stock.innerHTML = localStorage.getItem('mySQL');
+                        break;
+                    default:
+                        break;
+                }
+        */
     });
-
 }
+
 
 function removeFromCart(e) {
     let suppr = e.target.parentElement.parentElement;
@@ -132,8 +197,4 @@ function removeFromCart(e) {
             break;
     }
     suppr.remove();
-
-    console.log(e.target.parentElement.parentElement.innerText);
-
-
 }
