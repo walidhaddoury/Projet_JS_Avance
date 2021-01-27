0f1df09cc511ee9b80d8d51227525e02834b638b
walid <walidhaddoury@outlook.fr>
2021-01-27 09:34:23 +0100

    ajout du debut de projet du PO

---
+++ ProjetDepart/data/courses.js
@@ -0,0 +1,7 @@
+const COURSES = {
+  1: {id: 1, img: 'ux_ui.jpg', title: 'UX/UI', initial_price: 200, price: 9.99, mark: 4, stock: 10},
+  2: {id: 2, img: 'php_8.png', title: 'PHP 8', initial_price: 200, price: 9.99, mark: 3, stock: 10},
+  3: {id: 3, img: 'react_js.png', title: 'React JS', initial_price: 200, price: 9.99, mark: 2, stock: 5},
+  4: {id: 4, img: 'node_js.jpg', title: 'Node JS', initial_price: 200, price: 9.99, mark: 5, stock: 3},
+  5: {id: 5, img: 'my_sql.png', title: 'MySQL', initial_price: 200, price: 9.99, mark: 4, stock: 2}
+}
