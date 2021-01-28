7ec21e62628fac2ce6635cbf85991d62f942b6c7
walid <walidhaddoury@outlook.fr>
2021-01-28 19:23:41 +0100

    debut de la story 11

--- ProjetDepart/js/app.js
+++ ProjetDepart/js/app.js
@@ -1,10 +1,11 @@
-document.addEventListener('DOMContentLoaded', loadLocalStorage);
-
-const cartButton = document.querySelectorAll('.add-to-cart'); // All buttons Add To Cart
+document.addEventListener('DOMContentLoaded', loadLocalStorage); // Au chargement de la page lance la fonction LoadLocalStorage pour mettre a jour le panier
+//document.addEventListener('DOMContentLoaded', loadStockJSON);
+const allButtonAddToCart = document.querySelectorAll('.add-to-cart'); // All buttons Add To Cart
 const emptyCart = document.querySelector('#empty-cart');  // Button to reset Cart
 const listCart = document.querySelector('#cart-table tbody'); // liste of product in Cart
 
-let panier = JSON.parse(localStorage.getItem('Cart')) || [];
+let panier = JSON.parse(localStorage.getItem('Cart')) || []; // Initialise
+let stockJSON = JSON.parse(localStorage.getItem('Stocks')) || [];
 
 const productsTitle = document.querySelectorAll('.course__item .info__card h4');
 const productsImg = document.querySelectorAll('.course__item .course_img img');
@@ -13,12 +14,12 @@ const productsPrice = document.querySelectorAll('.course__item .info__card .disc
 const body = document.querySelector('body');
 const notifContainer = document.createElement('ul');
 
-const allStocks = document.querySelectorAll('.stock');
 
+emptyCart.addEventListener('click', clearLocalStorage);
 
 // Load the cart with localStorage
 function loadLocalStorage() {
-    if (panier) {
+    if (panier.length) {
         for (let i = 0; i < panier.length; i++) {
             let getTitle = panier[i].title;
             let getPrice = panier[i].price;
@@ -57,74 +58,136 @@ function loadLocalStorage() {
             listCart.appendChild(newProduct);
 
         }
+        loadStockJSON();
     }
     else {
         console.log("Le panier dans le LocalStorage est vide !");
     }
 }
 
+function loadStockJSON() {
+    if (panier.length) {
+        let uxNb = 0;
+        let phpNb = 0;
+        let reactNb = 0;
+        let nodeNb = 0;
+        let sqlNb = 0;
+        for (let i = 0; i < panier.length; i++) {
+            switch (i) {
+                case panier[i].title === "Node JS":
+                    nodeNb++;
+                    break;
+                case panier[i].title === "React JS":
+                    reactNb++;
+                    break;
+                case panier[i].title === "PHP 8":
+                    phpNb++;
+                    break;
+                case panier[i].title === "UX/UI":
+                    uxNb++;
+                    break;
+                case panier[i].title === "MySQL":
+                    sqlNb++;
+                    break;
+                default:
+                    break;
+            }
+        }
+        console.log("NODE", nodeNb);
+        let stock = {
+            uiUx: uxNb,
+            php: phpNb,
+            react: reactNb,
+            node: nodeNb,
+            sql: sqlNb
+        }
+    
+        stockJSON.push(stock);
+        localStorage.setItem('Stocks', JSON.stringify(stockJSON));
 
-emptyCart.addEventListener('click', clearLocalStorage);
+    } else {
+        console.log("Les stocks sont plein !");
+    }
+}
 
 
-for (let i = 0; i < cartButton.length; i++) {
+for (let i = 0; i < allButtonAddToCart.length; i++) {
 
     const titleCart = productsTitle[i].innerText;
     const priceCart = productsPrice[i].innerText;
     const imgCart = productsImg[i].getAttribute('src');
 
 
-    cartButton[i].addEventListener('click', () => {
+    allButtonAddToCart[i].addEventListener('click', () => {
 
-        let article = {
-            img: imgCart,
-            title: titleCart,
-            price: priceCart,
-            stock: 1
-        };
-        panier.push(article);
-
-        localStorage.setItem('Cart', JSON.stringify(panier));
-
-
-        const newProduct = document.createElement('tr');
-
-        let imgProduct = document.createElement('td');
-        let img = document.createElement('img');
-
-        let titleProduct = document.createElement('td');
-
-        let priceProduct = document.createElement('td');
-
-        let removeProduct = document.createElement('a');
-        removeProduct.addEventListener('click', removeFromCart);
-        let imgRemove = document.createElement('img');
-
-        let stock = document.createElement('td');
-        stock.innerHTML = 1;
-
-        img.setAttribute('src', productsImg[i].getAttribute('src'));
-        imgProduct.appendChild(img);
-
-        imgRemove.setAttribute('src', './img/fermer.svg');
-        removeProduct.appendChild(imgRemove);
-
-        titleProduct.innerHTML = titleCart;
-        priceProduct.innerHTML = priceCart;
-
-        newProduct.appendChild(imgProduct);
-        newProduct.appendChild(titleProduct);
-        newProduct.appendChild(priceProduct);
-        newProduct.appendChild(stock);
-        newProduct.appendChild(removeProduct);
-
-        listCart.appendChild(newProduct);
-
-        console.log(allStocks[i].innerText);
-
-        displayNotif(titleCart, "ajout");;
+        createJSON(imgCart, titleCart, priceCart);
+        createHTML(titleCart, priceCart, imgCart);
+        displayNotif(titleCart, "ajout");
+        updateStock(i);
+        loadStockJSON();
 
     });
+}
+
+
+function createJSON(imgCart, titleCart, priceCart) {
+    let article = {
+        img: imgCart,
+        title: titleCart,
+        price: priceCart,
+        stock: 1
+    };
+    panier.push(article);
+
+    localStorage.setItem('Cart', JSON.stringify(panier));
+}
+
+/*
+function createStockJSON(uxNb, phpNbNb, reactNb, nodeNb, sqlNb) {
+    let stock = {
+        uiUx: uxNb,
+        phpNb: phpNbNb,
+        react: reactNb,
+        node: nodeNb,
+        sql: sqlNb
+    }
+
+    stockJSON.push(stock);
+}*/
+
+function createHTML(titleCart, priceCart, imgCart) {
+    const newProduct = document.createElement('tr');
+
+    let imgProduct = document.createElement('td');
+    let img = document.createElement('img');
+
+    let titleProduct = document.createElement('td');
+
+    let priceProduct = document.createElement('td');
+
+    let removeProduct = document.createElement('a');
+    removeProduct.addEventListener('click', removeFromCart);
+    let imgRemove = document.createElement('img');
+
+    let stock = document.createElement('td');
+    stock.innerHTML = 1;
+
+    img.setAttribute('src', imgCart);
+    imgProduct.appendChild(img);
+
+    imgRemove.setAttribute('src', './img/fermer.svg');
+    removeProduct.appendChild(imgRemove);
+
+    titleProduct.innerHTML = titleCart;
+    priceProduct.innerHTML = priceCart;
+
+    newProduct.appendChild(imgProduct);
+    newProduct.appendChild(titleProduct);
+    newProduct.appendChild(priceProduct);
+    newProduct.appendChild(stock);
+    newProduct.appendChild(removeProduct);
+
+    listCart.appendChild(newProduct);
 }
 
 
@@ -139,7 +202,9 @@ function removeFromCart(e) {
 
     localStorage.setItem('Cart', JSON.stringify(panier));
 
-    displayNotif(supprAtttribute[1].innerHTML, "supression");
+    displayNotif(supprAtttribute[1].textContent, "supression");
+
+    updateStockDelete(supprAtttribute[1].textContent);
 
     //console.log(test);
     //console.log(panier.indexOf(supprAtttribute[1].innerHTML));
@@ -151,7 +216,9 @@ function clearLocalStorage() {
     //localStorage.clear();
     displayNotif("", "");
     panier = [];
+    stockJSON = [];
     localStorage.setItem('Cart', JSON.stringify(panier));
+    localStorage.setItem('Stocks', JSON.stringify(stockJSON));
     listCart.remove();
     document.location.reload();
 }
@@ -190,3 +257,31 @@ function displayNotif(title, event) {
     }, 3000);
 
 }
+
+function updateStock(index) {
+    const allStock = document.querySelectorAll('.stock');
+
+    let nb = (parseInt(allStock[index].textContent));
+
+    allStock[index].textContent = nb - 1;
+}
+
+
+function updateStockDelete(type) {
+    const allStock = document.querySelectorAll('.stock');
+
+    let index = null;
+
+    for (let i = 0; i < productsTitle.length; i++) {
+        console.log(productsTitle[i]);
+        if (productsTitle[i].textContent === type) {
+            index = i;
+            break;
+        }
+    }
+
+    let nb = (parseInt(allStock[index].textContent));
+
+    allStock[index].textContent = nb + 1;
+}
+
