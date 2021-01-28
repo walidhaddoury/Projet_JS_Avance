355b3618e4e0de05ae90cc89dc8e0f5509c2e90c
luxxx <lucas.liger@edu.itescia.fr>
2021-01-28 10:03:08 +0100

    search function added

---
+++ ProjetDepart/js/search.js
@@ -0,0 +1,18 @@
+let searchInput = document.querySelector('input');
+let allCard = document.querySelectorAll('.course__item');
+let allTitle = document.querySelectorAll('h4');
+
+searchInput.addEventListener('input', function(e) {
+    let input = searchInput.value;
+    input.toLowerCase();
+    for (let i = 0; i < allCard.length; i++) {
+        if (allTitle[i].innerText.toLowerCase().includes(input)) {
+            console.log(allTitle[i].innerText);
+            allCard[i].style.display = "block";
+        }else{
+            allCard[i].style.display = "none";
+        }
+        
+    }
+    console.log(allCard);
+});
