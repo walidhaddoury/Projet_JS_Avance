7813e0a771c69cdb0f9edff1e0b2d2f30600fda9
walid <walidhaddoury@outlook.fr>
2021-01-29 13:42:03 +0100

    si la recherche ne trouve rien le messag est bien display

--- ProjetDepart/js/search.js
+++ ProjetDepart/js/search.js
@@ -1,23 +1,26 @@
 let searchInput = document.querySelector('input');
 let allTitle = document.querySelectorAll('h4');
+let allCard = document.querySelectorAll('.course__item');
+let hidden = document.querySelector('#no_course');
 
 searchInput.addEventListener('keyup', function(e) {
-    let allCard = document.querySelectorAll('.course__item');
-    let hidden = document.querySelector('#no_course');
-
     let input = searchInput.value;
     input.toLowerCase();
 
+    let count = 0;
     for (let i = 0; i < allCard.length; i++) {
         if (allTitle[i].innerText.toLowerCase().includes(input)) {
             allCard[i].style.display = "block";
         }else{
             allCard[i].style.display = "none";
+            count++;
             
         }
-        if (allCard.length){
-            hidden.className = "pop";
-        }
     }
-
+    if (count === allCard.length){
+        hidden.className = "pop";
+    }
+    else {
+        hidden.className = "hidden";
+    }
 });
